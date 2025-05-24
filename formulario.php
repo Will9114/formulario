753cee1a5176da15453f30e53b8cc93b797f3a82
Will9114 <waqq9114@gmail.com>
2025-05-24 01:17:42 -0600

    Borrar sqlserver

--- formulario.php
+++ formulario.php
@@ -8,9 +8,3 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "rootits", "pwd" => "{your_password_here}", "Database" => "bd_PaaS", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:rootits.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
