6c7c0b8883be7d44261b55920f2792a870c47c62
Will9114 <waqq9114@gmail.com>
2025-05-24 01:21:49 -0600

    Prueba de formulario

--- formulario.php
+++ formulario.php
@@ -1,10 +1,22 @@
+<form method="POST">
+  Nombre: <input name="nombre"><br>
+  Correo: <input name="correo"><br>
+  <input type="submit" value="Enviar">
+</form>
+
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:rootits.database.windows.net,1433; Database = bd_PaaS", "rootits", "@Lfonso279114");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    try {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $conn = new PDO("sqlsrv:server = tcp:rootits.database.windows.net,1433; Database = bd_PaaS", "rootits", "@Lfonso279114");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("INSERT INTO usuarios (nombre, correo) VALUES (?, ?)");
+        $stmt->execute([$_POST['nombre'], $_POST['correo']]);
+        echo "Datos enviados.";
+        }
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
+?>
